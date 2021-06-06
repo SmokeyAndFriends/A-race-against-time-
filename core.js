de1154f8d550017fe5329a110009f4bb4bb89008
Ethan Belcher <pandurrerock@gmail.com>
2021-06-06 01:16:50 +0000

    here goes nothin'

--- core.js
+++ core.js
@@ -1,14 +1,14 @@
 const c = document.getElementById("#clicker");
 const l = document.getElementById("#label");
 
-var year = -500000;
+var year = 500000;
 var bcad = "bc";
 
 c.addEventListener("click", function() {
     year += 1
     if (year >= 1) {
         bcad = "AD"
-    } else(year <= 1); {
+    } else(year <= 1) {
         bcad = "bc"
     }
     l.innerHTML = "Year: " + year + bcad
