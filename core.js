3f3ce963f737cc8f5ccffee8603bd14f03cbdcff
Ethan Belcher <pandurrerock@gmail.com>
2021-06-08 19:41:53 +0000

    1.?.1

--- core.js
+++ core.js
@@ -1,14 +1,10 @@
-window.onload = DOM()
-
 var year = -500000;
 var bcad = "BC";
 
-function DOM() {
-    const c = document.getElementById("#c");
-    const l = document.getElementById("#l");
+const c = document.getElementById("#c");
+const l = document.getElementById("#l");
+const t = document.getElementsByClassName(".U1")[0];
 
-    const t = document.getElementsByClassName(".U1")[0];
-};
 
 if (c) {
     c.addEventListener("click", function() {
@@ -21,10 +17,10 @@ if (c) {
         }
         l.innerHTML = "Year: " + Math.abs(year) + bcad;
     });
-};
+}
 
 if (t) {
-    tl.addEventListener("click", function() {
-
+    t.addEventListener("click", function() {
+        window.onload = console.log("t is defined!");
     });
-};
+}
